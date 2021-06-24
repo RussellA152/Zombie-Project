bbdf5c0f41cf6e9ca18628de8bbc82620e6d0299
RussellA152 <79068548+RussellA152@users.noreply.github.com>
2021-06-23 20:53:52 -0700

    more changes

--- Assets/Scripts/EventScripts/DoorTrigger.cs
+++ Assets/Scripts/EventScripts/DoorTrigger.cs
@@ -5,9 +5,59 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public int id;
+    public int doorPrice;
+    private bool inTrigger;
+    private bool wantsToBuyDoor;
+    private void Start()
+    {
+        inTrigger = false;
+        wantsToBuyDoor = false;
+    }
+    private void Update()
+    {
+        //if the player is inside the trigger hitbox of said Door Trigger, AND they are holding the 'F' key, then wantsToBuyDoor is set to true, otherwise it wil be set to false
+        if(inTrigger && Input.GetKey(KeyCode.F))
+        {
+            wantsToBuyDoor = true;
+        }
+        else if (!inTrigger || Input.GetKey(KeyCode.F) == false)
+        {
+            wantsToBuyDoor = false;
+        }
+    }
+    //there are hitboxes infront of each side of a buyable door called Door Trigger (s), if player enters the trigger hitbox of said Door Trigger, our event is called
+
+    //when player enters the Door Trigger, inTrigger is set to true
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
-        GameEvents.current.DoorwayTriggerEnter(id);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inTrigger = true;
+        }
+    }
+    //while the player is inside the Door Trigger, we check if wantsToBuyDoor is true, and if they have the suffiicent points to buy the door,
+    //if they pass the check, we execute the event and open the door, otherwise the door stays closed
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            //Debug.Log("Hold 'f' to open Door [Cost: " + doorPrice);
+            if(PlayerScore.pScore >= doorPrice && wantsToBuyDoor)
+            {
+                Debug.Log("You opened Door #" + id);
+                PlayerScore.pScore -= doorPrice;
+                GameEvents.current.DoorwayTriggerEnter(id);
+            }
+            else
+            {
+                Debug.Log("You didn't buy door");
+            }   
+        }    
+    }
+    //when player exits the Door Trigger, inTrigger is set to false
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false; 
     }
 }
