8c779ef659003641bbabb4b194021163866715e6
RussellA152 <79068548+RussellA152@users.noreply.github.com>
2021-11-14 02:21:41 -0800

    Added Scene fade transitioning (WIP)

--- Assets/Scripts/GameManager/MySceneHandler.cs
+++ Assets/Scripts/GameManager/MySceneHandler.cs
@@ -6,6 +6,9 @@ public class MySceneHandler : MonoBehaviour
 {
     public static MySceneHandler current;
 
+    public GameObject animator_object;
+    public Animator animator;
+   
     private void Awake()
     {
 
@@ -20,11 +23,16 @@ public class MySceneHandler : MonoBehaviour
         }
 
     }
+    private void Start()
+    {
+        //finds "Fade Animator" in the scene
+        FindFadeAnimator();
+    }
 
     public void ChangeScene(string scene)
     {
         //loads a new scene 
-        StartCoroutine(LoadYourAsyncScene(scene));
+        StartCoroutine(FadeOutToLoad(scene));
 
     }
     public void QuiteGame()
@@ -41,10 +49,27 @@ public class MySceneHandler : MonoBehaviour
         {
             yield return null;
         }
+        FindFadeAnimator();
         //when scene is done loading, turn off input for a few se
         yield return new WaitForSeconds(InputManager.InputTurnOnTimer);
         InputManager.IsInputEnabled = true;
 
+       
+
+    }
+    IEnumerator FadeOutToLoad(string scene)
+    {
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(LoadYourAsyncScene(scene));
+
+    }
+    void FindFadeAnimator()
+    {
+        if (GameObject.Find("Fade Animator") != null)
+            animator_object = GameObject.Find("Fade Animator");
+        if (animator_object != null)
+            animator = animator_object.GetComponent<Animator>();
     }
 
 }
